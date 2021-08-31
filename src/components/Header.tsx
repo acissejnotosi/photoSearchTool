b02036945bb57f10ca97246949eff799641a7d25
Jessica Isoton <jessicasampaio195@gmail.com>
2021-08-31 18:35:23 +0200

    added link on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import logo from "../resources/images/logo.png";
 import SearchBar from "./SearchBar";
 
 const Header = (): JSX.Element => {
+  const history = useHistory();
+  const handelClick = () => {
+    history.push("/home");
+  };
   return (
     <header className="header l-grid__header--wrapper">
       <div className="l-grid__header__content">
-        <a href="/">
+        <a href="/" onClick={handelClick}>
           <div className="header__logo">
             <img className="header__image" src={logo} alt="" />
           </div>
